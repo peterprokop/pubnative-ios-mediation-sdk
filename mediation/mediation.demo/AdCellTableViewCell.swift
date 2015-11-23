a83101346c5c84f08fff4b0e4b527ff5c2143a65
Mohit Kumar <mohit.kumar@daffodilsw.com>
2015-11-23 16:53:36 +0530

    Code related to network request for demo app implemented

--- mediation/mediation.demo/AdCellTableViewCell.swift
+++ mediation/mediation.demo/AdCellTableViewCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AdCellTableViewCell: UITableViewCell {
+class AdCellTableViewCell: UITableViewCell, PubnativeNetworkRequestDelegate {
     
     @IBOutlet weak var labelPlacementId         : UILabel!
     @IBOutlet weak var labelAdapterName         : UILabel!
@@ -25,7 +25,7 @@ class AdCellTableViewCell: UITableViewCell {
         cleanView()
         activityIndicator.hidden = false
         activityIndicator.startAnimating()
-        //TODO: Implementation pending
+        request.request.startRequestWithAppToken(request.appToken, placementID: request.placementID, delegate: self)
     }
     
     override func awakeFromNib() {
@@ -66,5 +66,26 @@ class AdCellTableViewCell: UITableViewCell {
             starRatingView.hidden = false
             //TODO: Load thumb and large images and star tracking this
         }
+    }
+    
+    func requestDidStart(request: PubnativeNetworkRequest!) {
+        print("AdCellTableViewCell : requestDidStart")
+    }
+    
+    func request(request: PubnativeNetworkRequest!, didLoad ad: PubnativeAdModel!) {
+        print("AdCellTableViewCell : request:didLoad:")
+        activityIndicator.stopAnimating()
+        activityIndicator.hidden = true
+        self.request.ad = ad
+        renderAd()
+    }
+    
+    func request(request: PubnativeNetworkRequest!, didFail error: NSError!) {
+        print("AdCellTableViewCell : request:didFail:")
+        KSToastView.ks_showToast("\(error.domain)");
+        activityIndicator.stopAnimating()
+        activityIndicator.hidden = true
+        self.request.ad = nil
+        cleanView()
     }
 }
